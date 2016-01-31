59e0f522ae1cdb590e73f9ec3a8d4195cbcd1b73
berTrindade <berTrindade@users.noreply.github.com>
2016-01-30 23:25:17 -0200

    Branch release Initial commit

--- src/main.c
+++ src/main.c
@@ -1,16 +1,16 @@
 
-/* Libraries */
+// Libraries
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Header */
+// Header
 #include "../headers/menu.h"
 
 int main(int argc, char **argv)
 {
     load_menu();
 
-/* EXIT_SUCCESS will be replaced by 0, when the program compiles. */
+// EXIT_SUCCESS will be replaced by 0, when the program compiles.
     return EXIT_SUCCESS;
 }
 
